33faddcc8bc8562a7bc8f7dedaed27d502ab6f1d
niscode <nishimura.s405@gmail.com>
2022-11-04 13:00:23 +0900

    hanazonoexpo地点登録

--- launch_tlcclient.sh
+++ launch_tlcclient.sh
@@ -3,10 +3,14 @@
 # (ATR net) atr-dev02 env /// for ATR-demo
 # python3 ~/teleco-v_scripts/wsc-json_atr-dev02.py "https://atr-dev02.ca-platform.org/api/login" "CA001" "CA001" "wss://atr-dev02-websocket.ca-platform.org" "10.186.42.46" "1890" "10.186.42.31" "11920"
 
-# (ATR net) atr-dev02 env /// for hanazono-demo 
+# (ATR net) atr-dev02 env /// for hanazono-demo
 # python3 ~/teleco-v_scripts/wsc-json-hanazono.py "https://atr-dev02.ca-platform.org/api/login" "CA001" "CA001" "wss://atr-dev02-websocket.ca-platform.org" "10.186.42.46" "1890" "10.186.42.31" "11920"
 # python3 ~/teleco-v_scripts/wsc-json-hanazono.py "https://atr-dev02.ca-platform.org/api/login" "CA002" "CA002" "wss://atr-dev02-websocket.ca-platform.org" "10.186.42.37" "1890" "10.186.42.75" "11920"
 
 # (md-wlan) hanzono env /// for hanazono-demo
-python3 ~/teleco-v_scripts/wsc-json-hanazono.py "https://hanazono.ca-platform.org/api/login" "CA001" "CA001" "wss://hanazono-websocket.ca-platform.org" "192.168.100.81" "1890" "192.168.100.82" "11920"
-# python3 ~/teleco-v_scripts/wsc-json-hanazono.py "https://atr-dev02.ca-platform.org/api/login" "CA001" "CA001" "wss://atr-dev02-websocket.ca-platform.org" "192.168.100.81" "1890" "192.168.100.82" "11920"
+# ---- CA001
+python3 ~/teleco-v_scripts/wsc-json-hanazono.py "https://hanazono.ca-platform.org/api/login" "CA001" "CA001" "wss://hanazono-websocket.ca-platform.org" "192.168.100.121" "1890" "192.168.100.122" "11920"
+# ---- CA002
+# python3 ~/teleco-v_scripts/wsc-json-hanazono.py "https://hanazono.ca-platform.org/api/login" "CA002" "CA002" "wss://hanazono-websocket.ca-platform.org" "192.168.100.121" "1890" "192.168.100.122" "11920"
+
+# python3 ~/teleco-v_scripts/wsc-json-hanazono.py "https://atr-dev02.ca-platform.org/api/login" "CA001" "CA001" "wss://atr-dev02-websocket.ca-platform.org" "192.168.100.121" "1890" "192.168.100.122" "11920"
